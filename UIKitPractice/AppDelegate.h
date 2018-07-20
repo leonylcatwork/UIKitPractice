c2b46e959fa3de1d332e6956f1e1e3c16fba8baa
lEoN <leonylcatwork@gmail.com>
2018-07-20 19:29:25 +0800

    Initial Commit

---
+++ UIKitPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIKitPractice
+//
+//  Created by leon on 20/07/2018.
+//  Copyright © 2018 Maimemo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIKitPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIKitPractice
+//
+//  Created by leon on 20/07/2018.
+//  Copyright © 2018 Maimemo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
